3073f7d70a60c5656399a93c5df51390585fee27
jimmy <houwang.chen@smr-nbhx.com>
2021-05-22 22:58:50 +0800

    idea整合githup测试

---
+++ src/main/java/com/jimmy/demo/Model.java
@@ -0,0 +1,16 @@
+package com.jimmy.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/*
+ *@authur: Administrator
+ *@DATE: 2021/5/22
+ *@description
+ */
+@Getter
+@Setter
+public class Model {
+    private String name;
+    private  int age;
+}
